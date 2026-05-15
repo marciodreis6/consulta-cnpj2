c11f72bbcd10615bb21de64fbe6a3900224b05af
marciodreis6 <marciodreis6@gmail.com>
2026-05-15 01:40:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_base64(file_path):
     with open(file_path, "rb") as f:
         return base64.b64encode(f.read()).decode()
 
-img = get_base64("fundo.PNG")
+img = get_base64("fundo.png")
 
 st.markdown(
     f"""
